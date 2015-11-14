be65f69ed498016ce837eb8968da219ad4ff7165
kiran <kkanchamreddy@ebay.com>
2015-11-14 07:28:31 -0800

    Change to ViewHolder Pattern

--- app/src/main/java/com/kkanchamreddy/todo/ItemsAdapter.java
+++ app/src/main/java/com/kkanchamreddy/todo/ItemsAdapter.java
@@ -13,24 +13,35 @@ import java.util.ArrayList;
  * Created by kkanchamreddy on 11/10/2015.
  */
 public class ItemsAdapter extends ArrayAdapter<Item> {
-    public ItemsAdapter(Context context, ArrayList<Item> users) {
-        super(context, 0, users);
+
+    private static class ViewHolder {
+        TextView tvItemText;
+        TextView tvPriority;
+    }
+
+    public ItemsAdapter(Context context, ArrayList<Item> items) {
+        super(context, 0, items);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         Item item = getItem(position);
+
+        ViewHolder viewHolder = new ViewHolder();
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
         }
-        // Lookup view for data population
-        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
-        TextView tvPriority = (TextView) convertView.findViewById(R.id.tvPriority);
-        // Populate the data into the template view using the data object
-        tvName.setText(item.text);
-        tvPriority.setText(item.priority);
+
+        viewHolder.tvItemText = (TextView) convertView.findViewById(R.id.tvName);
+        viewHolder.tvPriority = (TextView) convertView.findViewById(R.id.tvPriority);
+
+        convertView.setTag(viewHolder);
+
+        viewHolder.tvItemText.setText(item.text);
+        viewHolder.tvPriority.setText(item.priority);
+       
         // Return the completed view to render on screen
         return convertView;
     }
